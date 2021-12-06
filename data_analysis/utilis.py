44a97b065c47eb7bab6759e291beb3c81d790752
MicasR <derciomicas@gmail.com>
2021-12-06 16:53:46 +0200

    added type check on utils

--- data_analysis/utilis.py
+++ data_analysis/utilis.py
@@ -3,12 +3,10 @@ from datetime import datetime
 
 def str_is_date(date_string: str, format: str = "%Y-%m-%d") -> bool:
     """Receive str return true if date, else return false."""
+    if type(date_string) != str: return False
     try:
         datetime.strptime(date_string, format)
         return True
     except ValueError:
         return False
-
-
-
 
