1246e24ab9a8bbdab30a1c465a91f597548449c3
MicasR <derciomicas@gmail.com>
2021-12-07 03:39:31 +0200

    Completed filter data function

--- data_analysis/data.py
+++ data_analysis/data.py
@@ -1,7 +1,8 @@
+import utils as u
 import pandas as pd
 
 
-def retrieve_data_from_csv(path:str, city:str) -> pd.DataFrame:
+def retrieve_data_from_csv(path: str, city: str) -> pd.DataFrame:
     """Get csv data and return a panda dataframe with origen column."""
     data = pd.read_csv(path)
     data["city"] = city
@@ -13,7 +14,7 @@ NEW_YORK = retrieve_data_from_csv("./raw_data/new_york_city.csv", "New York")
 WASHINGTON = retrieve_data_from_csv("./raw_data/washington.csv", "Washington")
 
 
-def casefold_str_items(listing:list)->list:
+def casefold_str_items(listing: list) -> list:
     """
     Receive a list and return a list with all string items casefolded
 
@@ -21,7 +22,7 @@ def casefold_str_items(listing:list)->list:
     ``casefold_str_items(['cHicago','WASHINGTON']) # ->['chicago','washington']``
     ``casefold_str_items(['ChicaGO','1', 2, True]) # ->['chicago','1', 2, True]``
     """
-    return [item.casefold() if type(item)==str else item for item in listing]
+    return [item.casefold() if type(item) == str else item for item in listing]
 
 
 def get_raw_by_city(cities: list[str]) -> pd.DataFrame:
@@ -48,12 +49,17 @@ def get_raw_by_city(cities: list[str]) -> pd.DataFrame:
     frames = []
     cities_lower_case = casefold_str_items(cities)
 
-    if "chicago" in cities_lower_case or "*" in cities_lower_case: frames.append(CHICAGO)
-    if "new york" in cities_lower_case or "*" in cities_lower_case: frames.append(NEW_YORK)
-    if "washington" in cities_lower_case or "*" in cities_lower_case: frames.append(WASHINGTON)
+    if "chicago" in cities_lower_case or "*" in cities_lower_case:
+        frames.append(CHICAGO)
+    if "new york" in cities_lower_case or "*" in cities_lower_case:
+        frames.append(NEW_YORK)
+    if "washington" in cities_lower_case or "*" in cities_lower_case:
+        frames.append(WASHINGTON)
 
-    if frames: return pd.concat(frames, ignore_index=True)
-    else: return pd.DataFrame()
+    if frames:
+        return pd.concat(frames, ignore_index=True)
+    else:
+        return pd.DataFrame()
 
 
 def get_clean_by_city(cities: list[str]) -> pd.DataFrame:
@@ -79,9 +85,100 @@ def get_clean_by_city(cities: list[str]) -> pd.DataFrame:
     ``get_clean_by_city([]) #-> no data``\n
     """
     df = get_raw_by_city(cities)
-    if pd.DataFrame().equals(df): return df
+    if pd.DataFrame().equals(df):
+        return df
     else:
         df["User Type"].fillna("unknown", inplace=True)
         df["Gender"].fillna("unknown", inplace=True)
         df["Birth Year"].fillna(0.0, inplace=True)
         return df
+
+
+filters = {
+    "cities": ["chicago", "washington", "new york"],  # list
+    "user_types": ['Subscriber', 'Customer', 'Dependent', 'unknown'],  # list
+    "genders": ["Male", "Famale", "unknown"],  # list
+    "date": {
+        "from": "2017-02-02",  # str "yyyy-mm-dd"
+        "thru": "2017-02-03",  # str "yyyy-mm-dd"
+    },
+    "duration": {
+        "min": None,  # float or float like
+        "max": 120   # float or float like
+    },
+    "age": {
+        "known": {
+            "include": True,  # bool
+            "min": 40,  # float
+            "max": 46,  # float
+        },
+        "unknown": {
+            "include": True,  # bool
+        }
+    }
+}
+
+
+def filter_data(filters: dict) -> pd.DataFrame:
+    """Receive filters as a dict and return a pandas Dataframe."""
+    # city
+    df = get_clean_by_city(filters["cities"])
+    if df.empty: return df
+
+    #User Type & Gender
+    filter_series = (
+        (df["User Type"].isin(filters["user_types"])) &
+        (df["Gender"].isin(filters["genders"]))
+    )
+
+    #Start Time
+    if u.str_is_date(filters["date"]["from"]):
+        filter_series = filter_series & (
+            pd.to_datetime(df["Start Time"], format='%Y-%m-%d') >= filters["date"]["from"]
+        )
+
+    #End Time
+    if u.str_is_date(filters["date"]["thru"]):
+        filter_series = filter_series & (
+            pd.to_datetime(df["End Time"], format='%Y-%m-%d') < filters["date"]["thru"]
+        )
+
+    #duration min
+    if u.is_positive_float(filters["duration"]["min"]):
+        filter_series = filter_series & (
+            df["Trip Duration"] >= float(filters["duration"]["min"])
+        )
+
+    #duration max
+    if u.is_positive_float(filters["duration"]["max"]):
+        filter_series = filter_series & (
+            df["Trip Duration"] < float(filters["duration"]["max"])
+        )
+
+
+    #known age
+    if not filters["age"]["known"]["include"]:
+        filter_series = filter_series & (
+            df["Birth Year"] == 0
+        )
+
+    #known age min
+    if u.is_positive_float(filters["age"]["known"]["min"]) and filters["age"]["known"]["include"]:
+        filter_series = filter_series & (
+            (df["Birth Year"] <= 2017-float(filters["age"]["known"]["min"]))
+        )
+
+    #known age max
+    if u.is_positive_float(filters["age"]["known"]["max"]) and filters["age"]["known"]["include"]:
+        filter_series = filter_series & (
+            (df["Birth Year"] > 2017-float(filters["age"]["known"]["max"])) |
+            (df["Birth Year"] == 0)
+        )
+
+    #unknown age
+    if not filters["age"]["unknown"]["include"]:
+        filter_series = filter_series & (
+            df["Birth Year"] != 0
+        )
+
+    return df[filter_series]
